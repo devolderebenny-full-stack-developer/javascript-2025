295e31cdd70f939d44b4efeecc0f26b6a132d2b9
Devoldere Benny <devolderebenny@gmail.com>
2025-09-24 12:29:58 +0200

    iets toegevoegd

--- Oefenbundel/Oef1/js/index.js
+++ Oefenbundel/Oef1/js/index.js
@@ -1,4 +1,4 @@
-//dit is een oefening
+//dit is een oefening de eerste
 for (let i = 1; i<=10; i++){
     console.log(i);
 }
--- oefenbundel2/Oef1/js/index.js
+++ oefenbundel2/Oef1/js/index.js
@@ -1,3 +1,6 @@
 for (let i = 1; i<=1000;i++){
+    if (i**2 <1000){
+        console.log(i**2);
+    }
 
 }
