e3a72fe5449d3df08f436e80fa69df34531db6b7
Devoldere Benny <devolderebenny@gmail.com>
2025-09-24 16:22:28 +0200

    oefeningen aangepast en nog een paar gemaakt

---
+++ oefenbundel2/Oef6/js/index.js
@@ -0,0 +1,7 @@
+let getal  = 2;
+var uitkomst = 0;
+for (let i = 1; i<=100; i++){
+   uitkomst = getal+1;
+   console.log(uitkomst);
+   getal = getal +2;
+}
---
+++ oefenbundel2/Oef7/js/index.js
@@ -0,0 +1,7 @@
+let getal  = 1;
+var uitkomst = 0;
+for (let i = 1; i<=100; i++){
+    uitkomst = getal+1;
+    console.log(uitkomst);
+    getal = getal +2;
+}
